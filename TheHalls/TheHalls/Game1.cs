82f1bc24f9e722720c8f3b8be15379ada83d5c5d
Henry <hco4630@g.rit.edu>
2021-03-14 23:30:12 -0400

    wrote code for screenoffset and world location vs screen location- in gameObject

---
+++ TheHalls/TheHalls/Game1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TheHalls
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Vector2 screenOffset;
+        private KeyboardState kb;
+
+        private Texture2D coinImg;
+        private SpriteFont arial16;
+
+        private GameObject testObj;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            testObj = new GameObject(new Vector2(50, 50), new Vector2(80, 96), coinImg);
+            screenOffset = new Vector2(75, 75);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            coinImg = Content.Load<Texture2D>("coin");
+            arial16 = Content.Load<SpriteFont>("arial16");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            kb = Keyboard.GetState();
+
+            //Currently, wasd moves the screenoffset.
+            if(kb.IsKeyDown(Keys.W))
+            {
+                screenOffset.Y--;
+            }
+
+            if(kb.IsKeyDown(Keys.S))
+            {
+                screenOffset.Y++;
+            }
+
+            if(kb.IsKeyDown(Keys.A))
+            {
+                screenOffset.X--;
+            }
+
+            if(kb.IsKeyDown(Keys.D))
+            {
+                screenOffset.X++;
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            testObj.Draw(_spriteBatch, screenOffset);
+            _spriteBatch.DrawString(arial16, "ScreenOffset: X: " + screenOffset.X + " Y: " + screenOffset.Y, new Vector2(25, 25), Color.Black);
+            
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TheHalls/TheHalls/GameObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace TheHalls
+{
+    /// <summary>
+    /// gameObject only takes a world location and size, and draw will adapt that to screen coordinates. 
+    /// </summary>
+    class GameObject
+    {
+        Vector2 worldLoc;
+        Vector2 size;
+        Texture2D image;
+
+        /// <summary>
+        /// creates a new gameObject at the given location with the given size and image.
+        /// </summary>
+        /// <param name="worldLoc"></param>
+        /// <param name="size"></param>
+        /// <param name="image"></param>
+        public GameObject(Vector2 worldLoc, Vector2 size, Texture2D image)
+        {
+            this.worldLoc = worldLoc;
+            this.size = size;
+            this.image = image;
+        }
+
+        /// <summary>
+        /// draws the object, adjusted to the screenOffset.
+        /// </summary>
+        /// <param name="sb"></param>
+        /// <param name="screenOffset"></param>
+        public void Draw(SpriteBatch sb, Vector2 screenOffset)
+        {
+            sb.Draw(image, new Rectangle((int)(worldLoc.X - screenOffset.X), (int)(worldLoc.Y - screenOffset.Y), (int)size.X, (int)size.Y), Color.White);
+        }
+    }
+}
