299738503b99960a80745ab7563dee739e4259c4
AJ Biswas <arjunjbiswas@outlook.com>
2021-03-15 14:37:33 -0400

    Updated Player with the weapon arc thing I changed a little bit of the Player class to be able to accomodate for this mechanic

--- TheHalls/TheHalls/Game1.cs
+++ TheHalls/TheHalls/Game1.cs
@@ -13,10 +13,14 @@ namespace TheHalls
         private KeyboardState kb;
 
         private Texture2D coinImg;
+        private Texture2D arcImg;
         private SpriteFont arial16;
 
         private GameObject testObj;
         private Player player;
+
+        private MouseState mouse;
+        private MouseState prevMouse;
 
         public Game1()
         {
@@ -32,7 +36,10 @@ namespace TheHalls
             base.Initialize();
 
             testObj = new GameObject(new Vector2(0, 0), new Vector2(80, 96), coinImg);
-            player = new Player(new Vector2(_graphics.PreferredBackBufferWidth/2 -20, _graphics.PreferredBackBufferHeight/2 - 24), new Vector2(40, 48), coinImg);
+            player = new Player(new Vector2(_graphics.PreferredBackBufferWidth/2 - 20, 
+                _graphics.PreferredBackBufferHeight/2 - 24), screenOffset,
+                new Vector2(40, 48), coinImg, arcImg);
+
             screenOffset = new Vector2(0, 0);
         }
 
@@ -40,8 +47,8 @@ namespace TheHalls
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             coinImg = Content.Load<Texture2D>("coin");
+            arcImg = Content.Load<Texture2D>("Arc");
             arial16 = Content.Load<SpriteFont>("arial16");
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
@@ -49,10 +56,17 @@ namespace TheHalls
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            player.Move();
+            // Get mouse and keyboard states
+            mouse = Mouse.GetState();
+            kb = Keyboard.GetState();
+
+            player.Move(kb);
+            player.Aim(mouse);
 
             //adjusts the screenOffset to center the player.
             screenOffset = new Vector2(player.WorldLoc.X - (_graphics.PreferredBackBufferWidth - player.Size.X) / 2, player.WorldLoc.Y - (_graphics.PreferredBackBufferHeight - player.Size.Y) / 2);
+
+            prevMouse = Mouse.GetState();
 
             base.Update(gameTime);
         }
@@ -63,7 +77,7 @@ namespace TheHalls
 
             _spriteBatch.Begin();
 
-            player.Draw(_spriteBatch, screenOffset);
+            player.Draw(_spriteBatch);
             testObj.Draw(_spriteBatch, screenOffset);
             _spriteBatch.DrawString(arial16, "ScreenOffset: X: " + screenOffset.X + " Y: " + screenOffset.Y, new Vector2(25, 25), Color.Black);
             
--- TheHalls/TheHalls/Player.cs
+++ TheHalls/TheHalls/Player.cs
@@ -9,38 +9,84 @@ namespace TheHalls
 {
     class Player : GameObject
     {
-
-        public Player(Vector2 worldLoc, Vector2 size, Texture2D image) : base(worldLoc, size, image)
+        private Vector2 arcLoc;
+        private Texture2D arcImg;
+        private float arcRotation;
+        private Vector2 offset;
+        public Player(Vector2 worldLoc, Vector2 screenOffset, Vector2 size, Texture2D image, Texture2D arcImage) : base(worldLoc, size, image)
         {
-
+            arcImg = arcImage;
+            arcRotation = 0;
+            offset = screenOffset;
         }
 
         /// <summary>
         /// Moves the character based on WASD
         /// </summary>
-        public void Move()
+        public void Move(KeyboardState kb)
         {
-            KeyboardState kb = Keyboard.GetState();
-
             if(kb.IsKeyDown(Keys.W))
             {
                 worldLoc.Y--;
+                offset.Y--;
             }
 
             if(kb.IsKeyDown(Keys.S))
             {
                 worldLoc.Y++;
+                offset.Y++;
             }
 
             if (kb.IsKeyDown(Keys.A))
             {
                 worldLoc.X--;
+                offset.X--;
             }
 
             if (kb.IsKeyDown(Keys.D))
             {
                 worldLoc.X++;
+                offset.X++;
             }
+        }
+
+        /// <summary>
+        /// Updates player's weapon slash arc vector and arc rotation
+        /// </summary>
+        /// <param name="mouse">Mouse cursor location</param>
+        public void Aim(MouseState mouse)
+        {
+            arcLoc = ScreenCenter + (Vector2.Normalize(new Vector2(mouse.X, mouse.Y) - ScreenCenter) * 100);
+
+            arcRotation = mouse.Y - ScreenCenter.Y > 0 ? 
+                (float)Math.Acos((arcLoc.X - ScreenCenter.X) / (arcLoc - ScreenCenter).Length()) + (float)(Math.PI / 2) :
+                -1 * (float)Math.Acos((arcLoc.X - ScreenCenter.X) / (arcLoc - ScreenCenter).Length()) + (float)(Math.PI / 2);
+        }
+
+        /// <summary>
+        /// Draws the player and the slash arc to the screen
+        /// </summary>
+        /// <param name="sb">SpriteBatch from Game1.Draw()</param>
+        public virtual void Draw(SpriteBatch sb)
+        {
+            // Draw player
+            base.Draw(sb, offset);
+
+            // Draw player weapon slash arc
+            sb.Draw(arcImg, new Rectangle((int)arcLoc.X,
+                (int)arcLoc.Y, image.Width, image.Height / 3),
+                new Rectangle(0, 0, arcImg.Width, arcImg.Height), Color.White,
+                arcRotation,
+                new Vector2(arcImg.Width / 2, arcImg.Height / 2), SpriteEffects.None, 0);
+        }
+
+        /// <summary>
+        /// Gets the center of the player's location 
+        /// relative to the camera (the pixel location)
+        /// </summary>
+        public Vector2 ScreenCenter
+        {
+            get { return (worldLoc - offset) + (Size / 2); } 
         }
     }
 }
