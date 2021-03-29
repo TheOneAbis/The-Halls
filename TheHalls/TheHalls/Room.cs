c6ab516d002cecf1fcde26c45fc419d123b57a64
Henry <hco4630@g.rit.edu>
2021-03-29 16:49:35 -0400

    started Room class

---
+++ TheHalls/TheHalls/Room.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TheHalls
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    class Room
+    {
+        protected Dictionary<Direction, Room> adjacentRooms;
+        protected List<GameObject> obstacles;
+        protected GameObject outDoor;
+        protected Direction outDirection;
+        protected Direction inDirection;
+        protected bool set;
+
+        public Room()
+        {
+
+        }
+    }
+}
